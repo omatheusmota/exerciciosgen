af59f89e41d1e1319b365e54ebe578d55a3b0275
Matheus F. Mota <mxtheusmota@gmail.com>
2022-07-12 21:24:21 -0300

    Adicionando pasta para exercícios de vetores e matrizes.

---
+++ java/atividade03/exercicio01.java
@@ -0,0 +1,35 @@
+package br.com.generation.vetoresematrizes;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+		/* 1. Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma
+		atividade e o escreva em seguida. Encontre após a maior pontuação e a apresente. */
+
+	
+		
+		int[] vetor = new int [5];
+		int maior;
+	
+		
+		System.out.println("Insira as 5 pontuações");
+			for (int x = 0; x <= 4; x++);{
+				System.out.println("Pontuação Nº" + ": ");
+				vetor [x] = entrada.nextInt();
+				entrada.nextLine();
+				
+				if (maiorPonto < vetor [x]) {
+					maiorPonto = vetor [x]
+				}
+		
+
+			}
+				
+				
+		}
+		
+	}
+
+
