74acfb3bade791060eedd54d8bea4a76852f1eb7
Matheus F. Mota <mxtheusmota@gmail.com>
2022-07-13 22:37:22 -0300

    Adicionando exercícios de classes.

---
+++ java/atividade04/exercicio01/classe01.java
@@ -0,0 +1,28 @@
+package br.com.generation.classes;
+
+public class classe01 {
+
+	
+		/* 1) Crie uma classe cliente e apresente os atributos e métodos referentes
+			esta classe, em seguida crie um objeto cliente, defina as instancias deste
+			objeto e apresente as informações deste objeto no console.*/
+
+		public static void main(String[] args) {
+			
+			
+			Cliente cli1 = new Cliente();
+			
+			cli1.setNome("Janet");
+			cli1.setTelefone("11977778888");
+			cli1.setEndereco("Rua do Amor, 92");
+
+			System.out.println("Dados do Cliente: ");
+			System.out.println("Nome: " + cl1.getNome());
+			System.out.println("Telefone: " + cl1.getTelefone());
+			System.out.println("Endereço: " + cl1.getEndereco());
+			
+			
+		}
+
+	}
+
